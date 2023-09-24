0affb25c2dedbc09e05886b6d0ff871251c656ce
Ummenthala Anish <ummenthala.anish@gmail.com>
2023-09-24 18:31:03 +0530

    Modified counter logic to framer-motion animations 🔀

--- src/components/atoms/CounterCard/index.tsx
+++ src/components/atoms/CounterCard/index.tsx
@@ -1,37 +1,72 @@
 'use client';
 
 import { FC, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 
-import CountUp from 'react-countup';
-import ScrollTrigger from 'react-scroll-trigger';
+import { animate } from 'framer-motion';
+import { motion } from 'framer-motion';
 import styles from './index.module.css';
 
 export interface CounterCardProps {
-  count: number;
+  from: number;
+  to: number;
   category: string;
 }
 
-const CounterCard: FC<CounterCardProps> = ({ count, category }) => {
-  const [counterStart, setCounterStart] = useState(false);
+const CounterCard: FC<CounterCardProps> = ({ from, to, category }) => {
+  const nodeRef = useRef<HTMLParagraphElement | null>(null);
+  const [isInView, setIsInView] = useState(false);
+
+  useEffect(() => {
+    const node = nodeRef.current;
+    if (!node) return;
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setIsInView(true);
+          }
+        });
+      },
+      { threshold: 0.1 }
+    );
+
+    observer.observe(node);
+
+    return () => {
+      observer.unobserve(node);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!isInView) return;
+
+    const node = nodeRef.current;
+    if (!node) return;
+
+    const controls = animate(from, to, {
+      duration: 1,
+      onUpdate(value) {
+        node.textContent = Math.round(value).toString();
+      },
+    });
+
+    return () => controls.stop();
+  }, [from, to, isInView]);
 
   return (
     <>
-      <ScrollTrigger onEnter={() => setCounterStart(true)}>
-        <div
-          className={`flex justify-start gap-2 p-8 h-32 desktop:h-36 desktop:p-12 tablet:px-4  text-center border border-gold shadow-boxGold rounded-xl ${styles.counterCard}`}
-        >
-          <div className="flex items-center justify-center w-2/6 p-2 text-sm font-semibold text-gold tablet:text-md desktop:text-lg">
-            {counterStart && (
-              <>
-                <CountUp start={0} end={count} duration={3} delay={0} /> +
-              </>
-            )}
-          </div>
-          <div className="flex items-center w-4/6 p-2 text-md font-semibold text-left tablet:text-sm desktop:text-lg">
-            {category}
-          </div>
+      <div
+        className={`flex justify-start gap-2 p-8 h-32 desktop:h-36 desktop:p-12 tablet:px-4  text-center border border-gold shadow-boxGold rounded-xl ${styles.counterCard}`}
+      >
+        <div className="flex items-center justify-center w-2/6 p-2 text-sm font-semibold text-gold tablet:text-md desktop:text-lg">
+          <motion.p ref={nodeRef} /> +
         </div>
-      </ScrollTrigger>
+        <div className="flex items-center w-4/6 p-2 text-md font-semibold text-left tablet:text-sm desktop:text-lg">
+          {category}
+        </div>
+      </div>
     </>
   );
 };
--- src/components/organisms/CounterSection/index.tsx
+++ src/components/organisms/CounterSection/index.tsx
@@ -7,9 +7,13 @@ const CounterSection: FC<CounterSectionProps> = ({}) => {
   return (
     <div className="p-12 tablet:p-10 desktop:py-16 desktop:pt-4 desktop:px-24">
       <div className="grid grid-cols-1 gap-12 py-16 mx-auto tablet:grid-cols-3 desktop:grid-cols-3">
-        <CounterCard category="Mathematicians Created" count={100} />
-        <CounterCard category="Hours Taught" count={2000} />
-        <CounterCard category="Communities and Schools partnered" count={5} />
+        <CounterCard category="Mathematicians Created" from={0} to={100} />
+        <CounterCard category="Hours Taught" from={0} to={2000} />
+        <CounterCard
+          category="Communities and Schools partnered"
+          from={0}
+          to={5}
+        />
       </div>
     </div>
   );
